7cf39a1a5dbfdc556ef82ca550f0a409d1b735d3
torxanic <i19014@hb.dhbw-stuttgart.de>
2021-10-29 17:19:19 +0200

    Play Sounds tests with simpleaudio

---
+++ TestAnwendungen/LEDSoundsTest.py
@@ -0,0 +1,16 @@
+import simpleaudio as sa
+import RPi.GPIO as io
+import time
+
+filename = '/home/pi/Projects/HelloWorldProject/TestAnwendungen/TestSounds/test.wav'
+led1 = 22
+
+io.setmode(io.BCM)
+io.setup(led1, io.OUT)
+
+
+wave_object = sa.WaveObject.from_wave_file(filename)
+io.output(led1, True)
+play_obj = wave_object.play()
+play_obj.wait_done() 
+io.output(led1, False)
