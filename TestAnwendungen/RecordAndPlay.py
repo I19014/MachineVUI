24bbd7a00216375ca90e6f96f2e6b95a0c165a5d
i19014 <i19014@hb.dhbw-stuttgart.de>
2021-11-01 20:06:19 +0100

    Finished RecordAndPlay

---
+++ TestAnwendungen/RecordAndPlay.py
@@ -0,0 +1,25 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as wv
+import simpleaudio as sa
+import numpy as np
+
+fs = 44100
+duration = 3.5
+filename = 'TestSounds/Ok.wav'
+
+#Record Voice
+recording = sd.rec(int(duration*fs),samplerate = fs, channels = 2)
+print('Start Recording')
+sd.wait()
+#write(filename,fs,recording.astype(np.int16))
+print('Finished Recording')
+wv.write(filename, recording, fs, sampwidth=2)
+
+#play recorded File
+#sd.play(filename)
+wave_object = sa.WaveObject.from_wave_file(filename)
+play_obj = wave_object.play()
+print('Play')
+play_obj.wait_done() 
+print('done')
